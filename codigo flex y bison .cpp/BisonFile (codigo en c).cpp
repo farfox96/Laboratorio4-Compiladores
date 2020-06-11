ab0bf80885775138f80203acf7ed41202c25bac8
jedeg08 <52443610+jedeg08@users.noreply.github.com>
2020-06-11 16:49:19 -0500

    Add files via upload

---
+++ codigo flex y bison .cpp/BisonFile (codigo en c).cpp
@@ -0,0 +1,59 @@
+%{
+/* fichero instrucciones.y */
+#include <stdio.h>
+  extern int yylex();
+  extern int yyparse();
+  extern FILE *yyin;
+ 
+  //void yyerror(const char *s);
+
+
+%}
+
+%token IDENTIFICADOR OPAS CONSTENTERA NL MAS APAR CPAR
+%start instrucciones
+
+
+%%
+instrucciones : instrucciones instruccion 
+              | instruccion
+;
+
+instruccion : IDENTIFICADOR OPAS expresion NL
+;
+
+expresion : termino
+          | expresion MAS termino
+
+termino : IDENTIFICADOR
+        | CONSTENTERA
+;
+        | APAR expresion CPAR
+;
+%%
+
+
+
+void yyerror (s) /* Llamada por yyparse ante un error */
+  char *s;
+{
+printf ("%s\n", s); /* Esta implementación por defecto nos valdra */
+/* Si no creamos esta funcion, habra que enlazar con ly en el
+momento de compilar para usar una implementacion por defecto */
+ printf ("Ingrese Enter para salir: ");
+getchar();
+}
+
+int main()
+{
+printf (" -- Bienvenidos al Laboratorio 4 de Diseno de Compiladores -- \n");
+printf (" - Ejemplo de analizador lexico-sintactico con Flex/Bison - \n");
+printf (" - Realizado por Jesus De Gracia y Daniel Gonzalez -\n");
+printf ("Cadena admitida por el analizador para usarla de ejemplo valido: a1:=5+a2\n\n");
+  //Acciones a ejecutar antes del analisis
+  printf ("\n\nIngrese valores para analizar >>\n ");
+  yyparse();
+ 
+  //Acciones a ejecutar después del analisis
+
+ }
---
+++ codigo flex y bison .cpp/FlexFile (codigo en c).cpp
@@ -0,0 +1,26 @@
+/*
+fichero instrucciones.l
+*/
+%{
+  #include <stdio.h>
+  #include "y.tab.h" /* GENERADO AUTOMÁTICAMENTE POR BISON */
+%}
+
+separador ([ \t""])+
+letra [a-zA-Z]
+digito [0-9]
+identificador {letra}({letra}|{digito})*
+constEntera {digito}({digito})*
+
+%%
+{separador} {/* omitir */};
+{constEntera} {printf(" constante "); return (CONSTENTERA);};
+":=" {printf(" asignacion "); return (OPAS);};
+"+" {printf(" suma "); return (MAS);};
+{identificador} {printf(" indentificador "); return (IDENTIFICADOR);};
+"(" {printf(" delim_a "); return (APAR);};
+")" {printf(" delim_b "); return (CPAR);};
+\n {printf(" new_line\n"); return (NL);};
+. ECHO;
+
+%%
